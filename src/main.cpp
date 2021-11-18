8d0845f5678c2e8b515eb1fe32d60210e129b202
Rupam Debnath <44625252+rupamdebnath@users.noreply.github.com>
2021-11-18 17:47:30 +0000

    Included a random generator with minimum and maximum values as input, this will be used for the randomly generated turns of enemies and also the  random values for attacks and heal.

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,19 @@
 #include "/home/runner/TheAncientLord/include/Player.hpp"
 using namespace std;
 
+int random(int min, int max) //range : [min, max]
+{
+  bool first = true;
+  if (first) 
+  {  
+    srand(time(NULL)); //seeding for the first time only!
+    first = false;
+  }
+  int _value = min + rand() % (( max + 1 ) - min);
+  return _value;
+}
 
 int main(int argc, char *argv[]) {
-
   // Keyboard inputs, will add depending on what keys are required in our game
   // Keyboard kb;
 	// kb.WhiteListKeys({'w', 'a', 's', 'd', ';', 0x0A, 27});
