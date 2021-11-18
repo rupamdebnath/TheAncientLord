d753897dbecf822cd376fc4375395010d4f1b034
Rupam Debnath <44625252+rupamdebnath@users.noreply.github.com>
2021-11-18 11:06:50 +0000

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+// #include "input.hpp"
+
+int main(int argc, char **argv) {
+	std::cout << "Hello world!" << std::endl;
+	return 0;
+}
+
+
+
+// int main() {
+// 	Keyboard kb;
+// 	//kb.WhiteListKeys({'w', 'a', 's', 'd', ';'});
+
+// 	while (kb.GetPressedKey() != 0x0A && kb.GetPressedKey() != 27){
+// 		kb.Update();
+// 		std::cout << kb.GetPressedKey();
+// 	}
+// }
